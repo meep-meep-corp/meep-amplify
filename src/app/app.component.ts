1812bbe21b597e3f123bec06bd8b3385d665660b
Nuno Guilherme Matos de Barros <nunobarros_@hotmail.com>
2019-06-21 23:31:23 +0200

    qr code on a separate page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'QR Code',
+      url: '/qr',
+      icon: 'code'
     }
   ];
 
