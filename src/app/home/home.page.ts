861702a1320e5748711dffaaa6287a688336f12f
Antonio Romero <antonio@orvium.io>
2019-06-22 15:04:26 +0200

    Remove analytics

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import awsconfig from '../../aws-exports';
-import {AmplifyService} from 'aws-amplify-angular';
 
 @Component({
   selector: 'app-home',
@@ -9,22 +7,9 @@ import {AmplifyService} from 'aws-amplify-angular';
 })
 export class HomePage {
   title = 'amplify-angular-app';
-  url = 'https://console.aws.amazon.com/pinpoint/home/?region='
-    + awsconfig.aws_project_region + '#/apps/'
-    + awsconfig.aws_mobile_analytics_app_id + '/analytics/events';
-  eventsSent = 0;
-  analyticsEventSent = false;
 
 
-  constructor(private amplifyService: AmplifyService) {
-  }
-
-  handleAnalyticsClick() {
-    this.amplifyService.analytics().record('AWS Amplify Tutorial Event')
-      .then((evt) => {
-        ++this.eventsSent;
-        this.analyticsEventSent = true;
-      });
+  constructor() {
   }
 
 }
