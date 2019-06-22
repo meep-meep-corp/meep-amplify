b3a43e2291ed388548a7f252c75929f09533440d
Nuno Guilherme Matos de Barros <nunobarros_@hotmail.com>
2019-06-22 02:29:37 +0200

    custom marker

--- src/app/maps/maps.page.ts
+++ src/app/maps/maps.page.ts
@@ -6,7 +6,8 @@ import {
   CameraPosition,
   MarkerOptions,
   Marker,
-  Environment
+  Environment,
+  BaseArrayClass
 } from '@ionic-native/google-maps';
 import { Platform } from '@ionic/angular';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
@@ -36,6 +37,31 @@ export class MapsPage implements OnInit {
         tilt: 30
       }
     });
+
+    let POINTS: BaseArrayClass<any> = new BaseArrayClass<any>([
+      {
+        position: { lat: 43.0741704, lng: -89.3809802 },
+        iconData: {
+          url: "https://www.pinclipart.com/picdir/middle/104-1044515_economy-car-svg-png-icon-free-download-538848.png",
+          size: {
+            width: 24,
+            height: 24
+          }
+        } 
+      },
+    ]);
+
+    POINTS.forEach((data: any) => {
+      data.disableAutoPan = true;
+      let marker: Marker = this.map.addMarkerSync(data);
+      marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(this.onMarkerClick);
+      marker.on(GoogleMapsEvent.INFO_CLICK).subscribe(this.onMarkerClick);
+      marker.setIcon(marker.get('iconData'));
+    });
+  }
+
+  onMarkerClick(params: any) {
+
   }
 
 }
