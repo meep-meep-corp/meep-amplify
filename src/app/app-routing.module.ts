83c3d28c6876986c226b7831e51b9ff17147021c
Nuno Guilherme Matos de Barros <nunobarros_@hotmail.com>
2019-06-22 02:13:09 +0200

    maps page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'maps', loadChildren: './maps/maps.module#MapsPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/maps/maps.page.ts
@@ -0,0 +1,41 @@
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
+import { Platform } from '@ionic/angular';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.page.html',
+  styleUrls: ['./maps.page.scss'],
+})
+export class MapsPage implements OnInit {
+  map: GoogleMap;
+  constructor(private platform: Platform) { }
+
+  async ngOnInit() {
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+  loadMap() {
+    this.map = GoogleMaps.create('map_canvas', {
+      camera: {
+        target: {
+          lat: 43.0741704,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    });
+  }
+
+}
